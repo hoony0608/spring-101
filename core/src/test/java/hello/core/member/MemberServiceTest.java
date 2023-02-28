0de1c22ceddb0ba9ff11b122bf8026bdf09339f9
hoony0608 <rhdqndyd0608@gmail.com>
2023-02-28 20:36:36 +0900

    싱글톤 컨테이너 - @Configuration과 싱글톤

--- core/src/test/java/hello/core/member/MemberServiceTest.java
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -2,28 +2,28 @@ package hello.core.member;
 
 import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberServiceTest {
 
     MemberService memberService;
 
-    @BeforeEach
-    public void beforeEach() {
-        AppConfig appConfig = new AppConfig();
-        memberService = appConfig.memberService();
-    }
 
     @DisplayName("join/find Test")
     @Test
     void join() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+
         // given: ~이 주어지고
         Member member = new Member(1L, "memberA", Grade.VIP);
 
         // when: ~일 때
         memberService.join(member);
+
         Member findMember = memberService.findMember(1L);
 
         // then: ~하게 된다
