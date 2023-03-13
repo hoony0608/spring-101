c399b20eae0c0c7cbbc18c66d631617cf723b632
hoony0608 <rhdqndyd0608@gmail.com>
2023-03-13 17:48:46 +0900

    의존관계 자동 주입 - @Autowired 필드 명, @Qualifier, @Primary

--- core/src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -9,7 +9,6 @@ public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000;
 
-
     @Override
     public int discount(Member member, int price) {
         if (member.getGrade() == Grade.VIP) {
