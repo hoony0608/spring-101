ae5eacf2182acc3019bfbd8c97fa9a6d55e9f40a
hoony0608 <rhdqndyd0608@gmail.com>
2023-03-13 16:46:20 +0900

    의존관계 자동 주입 - 조회 빈이 2개 이상 - 문제

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class OrderServiceImpl implements OrderService{
-
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy ;
 
