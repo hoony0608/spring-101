e345828fa41376db27fc5ae5fb29f9b6b9609bb2
hoony0608 <rhdqndyd0608@gmail.com>
2023-03-13 18:12:02 +0900

    의존관계 자동 주입 - 애노테이션 직접 만들기

---
+++ core/src/main/java/hello/core/annotation/MainDiscountPolicy.java
@@ -0,0 +1,14 @@
+package hello.core.annotation;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+// ctrl + n -> @qualifiy에서 복사
+@Qualifier("mainDiscountPolicy")
+public @interface MainDiscountPolicy {
+}
